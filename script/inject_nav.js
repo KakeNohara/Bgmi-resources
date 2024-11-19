9e0fdfdf537f7c52b627bcc4b0a7640f01be3023
KakeNohara <kanakjoshi474@gmail.com>
2024-11-20 01:08:34 +0530

    Update inject_nav.js

--- script/inject_nav.js
+++ script/inject_nav.js
@@ -1,8 +1,8 @@
 function initNav() {
-    fetch('nav.html')
-        .then(res => res.text())
+    fetch('nav.html')  // Fetches the content of the nav.html file
+        .then(res => res.text())  // Wait for the response and parse it as text
         .then(text => {
-            let oldelem = document.querySelector("nav#navbar");
-            oldelem.innerHTML = text;
+            let oldelem = document.querySelector("nav#navbar");  // Find the nav element
+            oldelem.innerHTML = text;  // Inject the nav content
         });
 }
