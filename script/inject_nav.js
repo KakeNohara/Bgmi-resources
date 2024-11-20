ace2ee4e88f4d97c49e39bb9c013fb243338681f
KakeNohara <kanakjoshi474@gmail.com>
2024-11-20 17:12:59 +0530

    Update inject_nav.js

--- script/inject_nav.js
+++ script/inject_nav.js
@@ -1,8 +1,12 @@
 function initNav() {
-    fetch('nav.html')  // Fetches the content of the nav.html file
-        .then(res => res.text())  // Wait for the response and parse it as text
-        .then(text => {
-            let oldelem = document.querySelector("nav#navbar");  // Find the nav element
-            oldelem.innerHTML = text;  // Inject the nav content
-        });
+    fetch('nav.html')
+        .then((res) => {
+            if (!res.ok) throw new Error("Failed to load nav.html");
+            return res.text();
+        })
+        .then((text) => {
+            const navElem = document.querySelector("nav#navbar");
+            navElem.innerHTML = text;
+        })
+        .catch((err) => console.error(err));
 }
